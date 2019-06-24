6e6ee2bfb043562602660e177ff905f2cf74ea17
Katana Tran <tkatyk21@gmail.com>
2019-06-24 02:40:14 +0000

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,10 +13,16 @@ def first_challenge
     }
   }
 
-  #your code here
-
-
-  #remember to return your newly altered contacts hash!
-  contacts
+contacts.each do |person, data|
+  data.each do |attribute, value|
+ if attribute == :favorite_icecream_flavors
+        value.delete_if {|flavor| 
+        flavor == "strawberry"}
+       
+    end
+  end 
 end
+contacts
+end 
+
 
